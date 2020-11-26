9a58a619440a0c2d15386e4979d5ed4c28b8eb72
贺爽 <45160925+heshuangxiaoyu@users.noreply.github.com>
2020-11-26 12:08:27 +0800

    heshuang

--- Project/myapp/src/shixinxiangmu/Search.js
+++ Project/myapp/src/shixinxiangmu/Search.js
@@ -1,23 +1,38 @@
 import React from 'react'
+import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
+import './search.css'
+
+
 const Search=(props)=>{
   
   
   
-    return (<div>
-      <SearchBar
-        value="宝贝"
-        placeholder="Search"
-        onSubmit={value => console.log(value, 'onSubmit')}
-        onClear={value => console.log(value, 'onClear')}
-        onFocus={() => console.log('onFocus')}
-        onBlur={() => console.log('onBlur')}
-        onCancel={() =>props.history.goBack()}
-        showCancelButton
-        // onChange={this.onChange}
-        // onCancel={()=>}
-      />
-    </div>);
-  
+  return (
+  <div>
+    <SearchBar
+      
+      placeholder="最佳热帖"
+      onSubmit={value => console.log(value, 'onSubmit')}
+      onClear={value => console.log(value, 'onClear')}
+      onFocus={() => console.log('onFocus')}
+      onBlur={() => console.log('onBlur')}
+      onCancel={() =>props.history.goBack()}
+      showCancelButton
+      
+      // onChange={this.onChange}
+      // onCancel={()=>}
+    />
+    <div class="bangdan">
+      <p>热搜榜单</p>
+    </div>
+    <div class='list'>
+      <ol>
+        <li><a>阿里云新品发布会</a></li>
+        <span><img src='./xiaohuomiao.png'/></span>
+      </ol>
+    </div>
+  </div>);
+
 }
 
 
