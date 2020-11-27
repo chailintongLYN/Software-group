85d7158143187f673833c3ec72e3651a4d30656c
贺爽 <45160925+heshuangxiaoyu@users.noreply.github.com>
2020-11-27 16:50:44 +0800

    heshuang

--- Project/myapp/src/shixinxiangmu/Login.js
+++ Project/myapp/src/shixinxiangmu/Login.js
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 // import { Redirect } from "react-router-dom"; //重定向使用
 
 import './login.css'
+let loginfo={
+    username:'',
+    password:''
+}
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "",
-            password: "",
-            rediectToReferrer: false, // 是否重定向之前的界面
+            password: ""
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSumit = this.handleSumit.bind(this);
@@ -20,28 +23,48 @@ class Login extends Component {
             this.setState({
                 username: e.target.value,
             });
+            loginfo.username=e.target.value;
         } else if (e.target.name === "password") {
             this.setState({
                 password: e.target.value,
             });
+            loginfo.password=e.target.value;
+            console.log(JSON.stringify(loginfo));
         }
+        
     }
     // 提交登录表单
     async handleSumit(e) {
         e.preventDefault();
+        console.log(e.target)
         const username = this.state.username;
         const password = this.state.password;
         if (username.length === 0 || password.length === 0) {
             alert("用户名或密码不能为空！");
             return;
         }
+        fetch('/data', {
+            method : 'POST',
+            headers : {
+              'content-type' : 'application/json'
+            },
+            body : JSON.stringify(loginfo)
+          }).then(res => res.json()).catch(err => {
+            alert(err.message)
+          }).then(response => {
+            if(response.status === 'success'){
+            //   location.href = '/login';
+              console.log(response)
+            }else{
+              alert('用户名或密码错误')
+            }
+          });
         // 保存信息到sessionStorage
         sessionStorage.setItem("username", username);
         // 登录成功后，设置redirectToReferrer为true;
         // this.setState({
         //     rediectToReferrer: true,
         // });
-        console.log(this.props.location)
         let RedirectUrl = this.props.location.state
             ? this.props.location.state.from.pathname
             : "/";
@@ -50,33 +73,37 @@ class Login extends Component {
     }
     render() {
         return (
-            
-            <div class="container">
-                <form onSubmit={this.handleSumit}>
-                    <div>
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="请输入用户名"
-                                value={this.state.username}
-                                onChange={this.handleChange}
-                                class="username"
-                            />
-                    </div>
-                    <div>
-                            
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="请输入密码"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                class="password"
-                            />
-                    </div>
-                    <input type="submit" value="登录" class="denglu" />
-                </form>
-            </div>
+            <body>
+                <div class="container">
+                    {/* <div class="ll"></div> */}
+                    <form onSubmit={this.handleSumit}>
+                        <div>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    placeholder="请输入用户名"
+                                    value={this.state.username}
+                                    onChange={this.handleChange}
+                                    class="username"
+                                />
+                        </div>
+                        <div>
+                                
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="请输入密码"
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                    class="password"
+                                />
+                        </div>
+                        <span class='tishi'>若您没有账号，请先注册</span>
+                        <input type="submit" value="登录" class="denglu" />
+                        <input type='botton' value="注册" class="zhuci"/>
+                    </form>
+                </div>
+            </body>
         );
     }
 }
