e56d505829f1373d505bd9dcc76b47261e7491ff
郝天恒 <826541313@qq.com>
2020-12-02 10:47:04 +0800

    别动这个

--- Project/Demo - hth/Demo/app.js
+++ Project/Demo - hth/Demo/app.js
@@ -98,7 +98,7 @@ app.post('/gethomedata',async c=>{
     //*还没写完
 })
 
-//获取我的文章 //前端代码有了可以删除
+//获取发表的文章 //前端代码有了可以删除
 app.get('/gettext',async c=>{
     c.res.body = fs.readFileSync('./gettext/index.html').toString('utf-8')
 })
@@ -115,15 +115,13 @@ app.post('/gettexts',async c=>{
         
         if(searchtype !=='title'){
             str = 'SELECT * FROM text where ' + searchtype + '=? '
-            console.log(1);
     
         }else{
             str = "SELECT * FROM text where title like '%"+ sevalue +"%'"
-            console.log(2);
         }
-        console.log(3);
         connection.query(str,[sevalue],function(error,results,fields){
             if(results.length == 0 ){
+                console.log(1);
                 resolve({'status': 'failed','code':'400'})
             }
             else{
@@ -262,5 +260,130 @@ app.post('/getmyfollowstext', async c=>{
     c.res.body = result;
     console.log(result)
 })
+
+
+//添加我的收藏的文章 //前端代码有了可以删除
+
+// app.get('/save',async c=>{
+//     c.res.body = index;
+// })
+
+//接口
+
+app.post('/addsave', async c => {
+    let {username, textid} = JSON.parse(c.body)
+    var result = await new Promise((resolve) => {
+        connection.query('INSERT INTO save VALUES(?,?)',[username,textid],function(error,results,fields){
+                if(results.length == 0 ){
+                    resolve({'status': 'failed','code':'400'})
+                }
+                else{
+                    resolve({'status':'success','results':results})
+                    console.log('插入成功');
+                }
+        })
+    }) 
+    connection.query('UPDATE text SET savenumber = savenumber+1 WHERE textid = ?',[textid],function (err, results){
+        if(err) throw err;
+        console.log('收藏+1');
+    })
+    c.res.body = result;
+    console.log(result)
+
+});
+
+//删除我收藏的文章 //前端代码有了可以删除
+
+// app.get('/save',async c=>{
+//     c.res.body = index;
+// })
+
+//接口
+
+app.post('/deletemysave', async c => {
+    let {username, textid} = JSON.parse(c.body)
+    var result = await new Promise((resolve) => {
+        connection.query('delete from save where username=? and textid=?',[username,textid],function(error,results,fields){
+                if(results.length == 0 ){
+                    resolve({'status': 'failed','code':'400'})
+                }
+                else{
+                    resolve({'status':'success','results':results})
+                    console.log('删除成功');
+                }
+        })
+    })
+    connection.query('UPDATE text SET savenumber = savenumber-1 WHERE textid = ?',[textid],function (err, results){
+            if(err) throw err;
+            console.log('收藏-1');
+    })
+    c.res.body = result;
+    console.log(result)
+});
+
+
+
+
+//获取我的收藏的文章 //前端代码有了可以删除
+app.get('/getsavetext', async c=>{
+    c.res.body = fs.readFileSync('./getsavetext/index.html').toString('utf-8');
+})
+
+//获取我的收藏的文章 的接口
+app.post('/getmysavetext', async c=>{
+    let username = JSON.parse(c.body)
+
+    let str = 'SELECT * FROM save where username =? ';
+
+    var result = await new Promise((resolve) => {
+        connection.query(str,username,function(error,results,fields){
+                if(results.length == 0 ){
+                    console.log(1);
+                    resolve({'status':'nosave','code':'400'})
+                }
+                else{
+                    resolve({'status':'success','results':results}) 
+                }
+        })
+    })
+    if(result.status === 'nosave'){
+        console.log(2);
+        c.res.body = result
+        return
+    }
+
+    let textidarr  =  []
+    
+    result.results.forEach((item,)=>{
+        textidarr.push(item.textid)
+    })
+    // console.log(textidarr)
+    
+    let strr = 'SELECT * FROM text where textid =? ';
+    
+    textidarr.forEach((item,index)=>{
+        if(index !==0){
+            strr = strr + 'or textid =? '
+        }
+    })
+
+    var result = await new Promise((resolve) => {
+        connection.query(strr,textidarr,function(error,results,fields){
+                if(results.length == 0 ){
+                    resolve({'status': 'failed','code':'400'})
+                }
+                else{
+                    resolve({'status':'success','results':results}) 
+                }
+        })
+    }) 
+
+
+    c.res.body = result;
+    console.log(result);
+})
+
+
+
 
 app.run(1234,2)
