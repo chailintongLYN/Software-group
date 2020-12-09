870902a1902290ac8d8cb814776e91b8e289bbeb
柴林彤 <44825541+chailintongLYN@users.noreply.github.com>
2020-12-09 10:15:22 +0800

    Add files via upload

---
+++ Project/后台管理系统/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Admin from './Pages/admin'
+import 'antd/dist/antd.css'; 
+function App() {
+  return (
+    <div className="App">
+       <Admin />
+    </div>
+  );
+}
+
+export default App;
